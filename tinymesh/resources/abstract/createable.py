2e174eefa47e4ef5431ff15016cd66928d882fde
Olav Frengstad <olav@tiny-mesh.com>
2018-03-03 16:56:29 +0100

    Specify apibase in Creatable resources

--- tinymesh/resources/abstract/createable.py
+++ tinymesh/resources/abstract/createable.py
@@ -3,8 +3,8 @@ from tinymesh.resources.abstract.resource import APIResource
 
 class CreateableResource(APIResource):
     @classmethod
-    def create(cls, body, auth=None, **params):
-        url = cls.resource_url(cls, params)
+    def create(cls, body, auth=None, apibase=None, **params):
+        url = cls.resource_url(cls, params, apibase=apibase)
         httpreq, resp = APIResource._post(cls, url, body, auth=auth)
 
         return cls._construct(resp._obj)
